7323d3b45b37837e7aa6d74b2b1fc301848b3217
Leechael Yim <yanleech@gmail.com>
2026-04-11 02:26:09 +0800

    feat: add mark-read command

---
+++ cmd/mava/mark_read.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/phalahq/mava-api/internal/api"
+	"github.com/spf13/cobra"
+)
+
+var markReadCmd = &cobra.Command{
+	Use:   "mark-read <ticket-id> <message-id>...",
+	Short: "Mark messages as read in a ticket",
+	Args:  cobra.MinimumNArgs(2),
+	RunE:  runMarkRead,
+}
+
+func init() {
+	rootCmd.AddCommand(markReadCmd)
+}
+
+func runMarkRead(cmd *cobra.Command, args []string) error {
+	ticketID := args[0]
+	messageIDs := args[1:]
+
+	client, err := api.NewClient()
+	if err != nil {
+		return err
+	}
+
+	if err := client.MarkMessagesRead(ticketID, messageIDs); err != nil {
+		return fmt.Errorf("API request failed: %w", err)
+	}
+
+	fmt.Printf("Marked %d message(s) as read in ticket %s\n", len(messageIDs), ticketID)
+	return nil
+}
